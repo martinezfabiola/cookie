b48da2976be8558c1a0df5d43a55ab40e5ef1ea5
yannicadjaffar <yannica.djaffar@epfedu.fr>
2018-09-11 16:57:16 +0200

    test after adding gitignore

--- src/main/java/com/example/cookie/CookieApplication.java
+++ src/main/java/com/example/cookie/CookieApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CookieApplication {
-//test comment mario
+//test comment yannica
 	public static void main(String[] args) {
 		SpringApplication.run(CookieApplication.class, args);
 	}
