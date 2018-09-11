291ed8e26be9b2f0f2564290a1236b013800a930
mmckeen1996 <mo.mckeeney@gmail.com>
2018-09-11 16:54:39 +0200

    test commit mario

--- src/main/java/com/example/cookie/CookieApplication.java
+++ src/main/java/com/example/cookie/CookieApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CookieApplication {
-//test comment
+//test comment mario
 	public static void main(String[] args) {
 		SpringApplication.run(CookieApplication.class, args);
 	}
