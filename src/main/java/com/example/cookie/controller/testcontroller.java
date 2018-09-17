bc67c75484ced20edba7adb1462ffeda32a7c8ac
yannicadjaffar <yannica.djaffar@epfedu.fr>
2018-09-17 11:26:10 +0200

    add testcontroller class

---
+++ src/main/java/com/example/cookie/controller/testcontroller.java
@@ -0,0 +1,30 @@
+package com.example.cookie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * TODO class details.
+ *
+ * @author Loïc Ortola on 10/09/2018
+ */
+@Controller
+public class testcontroller {
+
+    /**
+     * Ceci sera mappé sur l'URL '/users'.
+     * C'est le routeur de Spring MVC qui va détecter et appeler directement cette méthode.
+     * Il lui fournira un "modèle", auquel on pourra rajouter des attributs.
+     * Ce modèle sera ensuite forwardé à une page web (dans resources/templates).
+     * Le nom de la template est retourné par la fonction. Ici, elle appelle donc le template "users".
+     * @param model le modèle
+     * @return
+     */
+    @GetMapping("/hello")
+    public  String getUsers (Model model) {
+        return "hello-world";
+    }
+
+}
+
